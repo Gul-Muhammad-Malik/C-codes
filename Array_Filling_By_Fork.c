0b38d6f765adaf10931d58d5fd368e94aac0f7e1
Gul Muhammad <44026876+Gul-Muhammad-Malik@users.noreply.github.com>
2018-10-13 12:45:46 +0500

    Array

--- Array_Filling_By_Fork.c
+++ Array_Filling_By_Fork.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <sys/wait.h>
 
-int add=0;int strt=0;int end=100;
+int add;int strt=0;int end=100;
 
 void fill(int *arr)
 {
@@ -10,110 +11,126 @@ void fill(int *arr)
 		arr[i]=1;
 		sum+=arr[i];
 	}
-	add+=sum;
+	add=add+sum;
 	strt=end;
 	end=end+100;
 	
-	printf("%d \n",add);
 }
 
 int main(){
 
 	int arr[1000];
 	int cpid=-1;
-	cpid=fork();
 
-	 if(cpid<0)
+	cpid=fork();
+		printf("parent = %d \n",cpid);
+//	exit(add);
+if(cpid==0)
+{
+		 if(cpid<0)
 	{
  	  printf("\nChild not created\n");
- 	  exit(1);
+ 	  exit(0);
 	}
 	else if(cpid==0)
 	{
+			//exit(add);
 	  fill(arr);
  	  cpid=fork();
+				printf("1= %d \n",cpid);
 		if(cpid<0)
 		{
  		  printf("\nChild not created\n");
- 		  exit(1);
+ 		  exit(0);
 		}
 		else if(cpid==0)
 		{
 		  fill(arr);
  		  cpid=fork();
+		printf("2= %d \n",cpid);
 			if(cpid<0)
 			{
  			  printf("\nChild not created\n");
- 			  exit(1);
+ 			  exit(0);
 			}
 			else if(cpid==0)
 			{
 			 fill(arr);
  			  cpid=fork();
+				printf("3= %d \n",cpid);
 				if(cpid<0)
 				{
 			 	  printf("\nChild not created\n");
-			 	  exit(1);
+			 	  exit(0);
 				}
 				else if(cpid==0)
 				{
 				  fill(arr);
 			 	  cpid=fork();
+					printf("4= %d  \n",cpid);
 					if(cpid<0)
 					{
 				 	  printf("\nChild not created\n");
-				 	  exit(1);
+				 	  exit(0);
 					}
 					else if(cpid==0)
 					{
 					  fill(arr);
 				 	  cpid=fork();
+							printf(" 5=  %d\n",cpid);
 						if(cpid<0)
 						{
 					 	  printf("\nChild not created\n");
-					 	  exit(1);
+					 	  exit(0);
 						}
 						else if(cpid==0)
 						{
 						  fill(arr);
 					 	  cpid=fork();
+									printf("6= %d \n",cpid);
 							if(cpid<0)
 							{
 						 	  printf("\nChild not created\n");
-						 	  exit(1);
+						 	  exit(0);
 							}
 							else if(cpid==0)		
 							{
 							  fill(arr);			
 						 	  cpid=fork();
+										printf(" 7= %d\n",cpid);
 								if(cpid<0)
 								{
 							 	  printf("\nChild not created\n");
-							 	  exit(1);
+							 	  exit(0);
 								}
 								else if(cpid==0)
 							 	{
 								  fill(arr);
  								  cpid=fork();
+											printf("8= %d\n",cpid);
 								  	if(cpid<0)
    								        {
 								  	 printf("\nChild not created\n");
-								  	 exit(1);
+								  	 exit(0);
 								  	}
 								  	else if(cpid==0)
 								  	{
 								  	 fill(arr);
 								  	 cpid=fork();
+											printf(" 9= %d\n",cpid);
 										if(cpid<0)
 										{
  										  printf("\nChild not created\n");
- 										  exit(1);
+ 										  exit(0);
 										}
 										else if(cpid==0)
 										{
 	  									 fill(arr);
  	  									 cpid=fork();
+												printf(" 10= %d\n",cpid);
+			//								exit(add);
 										}
+		//								exit(add);
 									}
 								}
 							}
@@ -122,9 +139,29 @@ int main(){
 				}
 			}
 		}
+//		exit(add);
 	}	
-	
+	//exit(add);
+//return 1;
+}
+//else
+ if (cpid==0)
+{
+	//wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);	wait(NULL);
+	printf("%d \n",add);
+}
+//wait(NULL);
+	//wait(NULL);
+	//wait(NULL);
+//	wait(NULL);
+	//wait(NULL);
+	//wait(NULL);
+	//wait(NULL);
+	//wait(NULL);
+	//wait(NULL);
 return 0;
 }
+
+
 
 
